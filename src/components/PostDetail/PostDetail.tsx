c55db42589ab133207ed981bb1f2d406b471defa
Vinicius Ribeiro <vini.silvaa2002@gmail.com>
2025-09-07 19:46:20 -0300

    Mostra de posts melhorado na home e alterado lang para pt-br em index.html

--- src/components/PostDetail/PostDetail.tsx
+++ src/components/PostDetail/PostDetail.tsx
@@ -6,20 +6,29 @@ const PostDetail = ({ post }: { post: Post }) => {
   return (
     <div className={styles.post_detail}>
       <img src={post.image} alt={post.title} />
-      <h2>{post.title}</h2>
-      <p className={styles.createdby}>Por: [{post.createdBy}]</p>
-      <div className={styles.tags}>
-        {post.tags &&
-          post.tags.map((tag: string) => (
-            <p key={tag}>
-              <span>#</span>
-              {tag}
-            </p>
-          ))}
+
+      {/* Este contêiner é a chave para o layout flex funcionar */}
+      <div className={styles.post_content_container}>
+        {/* Agrupador para o conteúdo superior */}
+        <div>
+          <h2>{post.title}</h2>
+          <p className={styles.createdby}>Por: {post.createdBy}</p>
+          <div className={styles.tags}>
+            {post.tags &&
+              post.tags.map((tag: string) => (
+                <p key={tag}>
+                  <span>#</span>
+                  {tag}
+                </p>
+              ))}
+          </div>
+        </div>
+
+        {/* O Link fica no final graças ao justify-content: space-between do pai */}
+        <Link to={`/posts/${post.id}`} className="btn btn-outline">
+          Ver detalhes
+        </Link>
       </div>
-      <Link to={`/posts/${post.id}`} className="btn btn-outline">
-        Ler
-      </Link>
     </div>
   );
 };
